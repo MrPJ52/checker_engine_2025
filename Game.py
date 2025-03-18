f1e3060e547bbf0c3645043828168f753ff67b2b
MrPJ52 <myp0505@gmail.com>
2025-03-18 17:29:00 +0900

    Major update on attack method
    Detach method for one check to find its target, so that I can reuse it in main attack() method.

--- Game.py
+++ Game.py
@@ -54,19 +54,14 @@ class Game:
                     print("|" + self.board[y][x] + "|", end="")
             print()
     
-    def move_check(self):
+    # Method to move check in any location.
+    # Used to debug and test.
+    def move_debug(self):
         pos_start = str(list(map(int, input("Position of check you want to move (input as x,y): ").split(","))))
         pos_target = list(map(int, input("Position of tile you want to move to (input as x,y): ").split(",")))
 
         # Find check in start position
         if pos_start in self.checks_list.keys():
-            # if check is attacking, call attack() method
-            # if (str(pos_target) in self.checks_list.keys() \
-            # and self.checks_list[str(pos_target)].side != self.checks_list[pos_start].side):
-            #     self.attack(pos_start, pos_target)
-            #     return
-            
-
             self.checks_list[pos_start].move(pos_target)
             
             sleep(0.5)
@@ -85,32 +80,78 @@ class Game:
         print("There is no check in start position.\n")
         return
     
-    def get_atk_list(self):
+    def get_atk_dict(self):
     # Find if there is any piece attacking another piece
     # Returns Dictionary as following:
     # str(Position of attacker): [list of str(target position)]
         atk_check_dict = dict()
         for check in self.checks_list.values():
             if check.side == self.turn_player:
-                for posible_move in check.moves:
-                    pos_atk = str([check.pos[i] + posible_move[i] for i in range(2)])
+                # TODO: Separate method for each checks to check the targets
+                # It is to re-use the method in attack() method.
+                targets = self.find_targets(str(check.pos))
+                if targets:
                     try:
-                        # check if there is an enemy check in pos_atk
-                        # and the position across the enemy is empty
-                        if (self.checks_list[pos_atk].side != check.side) \
-                        and (self.board[check.pos[1] + 2*posible_move[1]][check.pos[0] + 2*posible_move[0]] == 0):
-                            # save in atk_check_dict
-                            try:
-                                atk_check_dict[(str(check.pos))].append(pos_atk)
-                            except:
-                                atk_check_dict[(str(check.pos))] = [pos_atk]
+                        for target in targets:
+                            atk_check_dict[(str(check.pos))].append(str(target))
                     except:
-                        pass
+                        atk_check_dict[(str(check.pos))] = targets
         
         return atk_check_dict
 
+    # Parameter: Dictionary that is result of get_atk_dict, Boolean to check player has already attacked
+    def attack(self, atk_dict, alreadyAtked):
+        if (not alreadyAtked):
+            print("Attackable checks are:")
+            for pos in atk_dict.keys():
+                print(pos, end="  ")
+            
+            print("Choose which one to start attack(Input nth): ", end="")
+            atkPosNum = int(input())
+            atkPos = atk_dict.keys()[atkPosNum]
+
+        # If attacker has one and only target, choose it automatically
+        if len(atk_dict[atkPos]) == 1:
+            targetPos = atk_dict[atkPos][0]
+        
+        #If attacker has multiple targets, choose which one to attack
+        else:
+            print("This attacker has following targets: ")
+            for targetPos in atk_dict[atkPos]:
+                print(targetPos, end="  ")
+            print("Choose the target(Input nth): ", end = "")
+
+            targetPosNum = int(input())
+            targetPos = atk_dict[atkPos][targetPosNum]
+        
+
+        print(f"Check in position {atkPos} attacks {targetPos}")
+        # Call captured() method of target and delete it
+        self.checks_list.pop(targetPos).captured()
+
+    # Parameter: str(Position of attacker)
+    def find_targets(self, checkPos):
+        check = self.checks_list[checkPos]
+        for posible_move in check.moves:
+            pos_atk = str([check.pos[i] + posible_move[i] for i in range(2)])
+            try:
+                # check if there is an enemy check in pos_atk
+                # and the position across the enemy is empty
+                if (self.checks_list[pos_atk].side != check.side) \
+                and (self.board[check.pos[1] + 2*posible_move[1]][check.pos[0] + 2*posible_move[0]] == 0):
+                    try:
+                        target_list.append(pos_atk)
+                    except:
+                        target_list = [pos_atk]
+            except:
+                pass
+
+            # Return list of str(position of target)
+            try:
+                return target_list
+            except:
+                return
+        
+
     def promote(self, pos_target):
         pass
-
-    def attack(self, pos_start, pos_target):
-        print("attack method has been called.\n")
--- main.py
+++ main.py
@@ -8,19 +8,19 @@ while True:
 
     # Find if there is any attacking checks
     # If there is, attack
-    if GameController.get_atk_list():
+    if GameController.get_atk_dict():
         GameController.attack()
-        print(GameController.get_atk_list())
+        print(GameController.get_atk_dict())
         
     # If there is not,
     # Find if there is any movable checks
     else:
-
     # If there is no movable checks,
     # Turn player is defeated, end game
+    # TODO: Create method to make player win or lose.
 
     # If there is movable checks, move
-        GameController.move()
+        GameController.move_debug()
 
     # Change turn player
     GameController.turn_player = 'W' if GameController.turn_player == 'B' else 'B'
