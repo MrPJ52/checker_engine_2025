6ad7223576745253352e5d05bd6bde48a9acc65c
MrPJ52 <myp0505@gmail.com>
2025-03-31 16:50:58 +0900

    Minor update in score_board()

--- heuristic_score.py
+++ heuristic_score.py
@@ -22,25 +22,28 @@ class ScoreFunction:
         return
 
     def score_board(self, stdSide, target_board:list):
-        w_center = 0.3
+        w_center = 1.3
         w_advanced = 1.5
-        w_left_moves = 0.2
+        w_left_moves = 1.2
         # In case of weight of number of check or King,
-        # the notation in board(1 or 2) is weight by itself.
+        # the notation in Game.board(1 or 2) is weight by itself.
 
         score_sum = 0
 
         for i in range(len(target_board)):
             tile = target_board[i]
-            # Transit from index to coordination
+            # Convert index to coordination
             y = int((i+1)/4)
             x = (i+1)%4 - (y%2)
+
             # Judge center
             if (2 <= x) and (x <= 5) and (2 <= y) and (y <= 5):
                 tile *= w_center
             # Judge advanced
             if (((y >= 6) and (stdSide == "B")) or ((y <= 1) and (stdSide == "W"))):
                 tile *= w_advanced
+            # Judge left moves
+            # TODO: Connect with Game.find_moves() to get weight of left moves.
             
             score_sum += tile
         
